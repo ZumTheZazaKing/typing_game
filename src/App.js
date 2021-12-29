c7962de7c916c882e604fae025b49ae467ab03ba
ZumTheZazaKing <zahidi85543@gmail.com>
2021-12-29 19:12:27 +0800

    Main Page complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { lazy, Suspense } from 'react';
+import { Context } from './context';
+
+const Main = lazy(() => import('./pages/Main').then(module => ({ default: module.Main })));
+
 function App() {
+
   return (
     <div className="App">
-      <h1>Work In Progress</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Context.Provider value={{}}>
+
+          <Main />
+
+        </Context.Provider>
+      </Suspense>
     </div>
   );
 }
---
+++ src/pages/Main.js
@@ -0,0 +1,49 @@
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useState } from 'react';
+
+export const Main = () => {
+
+    const [mainHelpOpen, setMainHelpOpen] = useState(false);
+
+    return (<div id="Main">
+        <div id="main-content">
+            <div>
+                <h1>Zum's Typing Game</h1>
+                <br/>
+                <Button variant="contained">Start</Button>
+            </div>
+            <IconButton id="help" onClick={() => setMainHelpOpen(true)}>
+                <QuestionMarkIcon />
+            </IconButton>
+            <Dialog
+                open={mainHelpOpen}
+                onClose={() => setMainHelpOpen(false)}
+            >
+                <DialogContent>
+                    <DialogContentText>
+                        1. Upon starting the game, you will be given a selection of difficulties.
+                        <br/><br/>
+                        2. After choosing a difficulty, you will be given a random word to type. 
+                        You must type that word correctly within the initial time.
+                        <br/><br/>
+                        3. If you succeed in typing the word, you will be given more time 
+                        (scales with difficulty) and a new word will be given for you to type next.
+                        <br/><br/>
+                        4. The game ends when the time runs out so it's only a matter of how long you
+                        last.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setMainHelpOpen(false)} autoFocus>Got it</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    </div>)
+}
